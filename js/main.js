f0222dee4236823c17707da9784b4f967773b310
ciosba-141 <stellozzo71@gmail.com>
2024-01-02 09:23:29 +0100

    fatti i primi due fetch

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
 // fetch API
- 
-const Url = 'https://api.teleport.org/api/' ;
-
-const options = {
-    headers: {
-        'Content-Type': 'application/json',
-    },
+const options = { 
+    headers: {'Content-Type': 'application/json',},
 }; 
 
-fetch(Url, options)
-.then(res=> {
-    console.log(res)
-    return res.json(); 
-})
-.then(res => {
-    console.log(res);
-})
-.catch(error =>{
-    console.error('si è verificato un errore durante la richiesta :', error.message)
-})
+function GetDati(data){
+    fetch(data, options)
+    .then(result=> { 
+        return result.json(); 
+    })
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error =>{
+        console.error('si è verificato un errore durante la richiesta :', error.message)
+    });
+}
+
+// fetch search city
+
+function getCity(city){
+    const Url = 'https://api.teleport.org/api/' ;
+
+    fetch(Url, options)
+    .then(result=> { 
+        return result.json(); 
+    })
+    .then(result => {
+        let data = result['_links']['city:search']['href'];
+        data = data.replace(/{.*}/,'');
+        data = data + "?search=" + city;
+        GetDati(data)
+    })
+    .catch(error =>{
+        console.error('si è verificato un errore durante la richiesta :', error.message)
+    });
+
+};
+
+getCity('Dublino');
+
